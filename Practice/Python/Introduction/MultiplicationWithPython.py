56ff819bdc73d78e0e822dbcc204c1a3728f780d
Manohar Singh <manohar.aswal@gmail.com>
2017-03-16 03:33:03 -0700

    Practice Python

---
+++ Practice/Python/Introduction/MultiplicationWithPython.py
@@ -0,0 +1,6 @@
+def main():
+    num1 = int(input())
+    num2 = int(input())
+    print(num1 * num2)
+
+main()
---
+++ Practice/Python/Introduction/SubtractionWithPython.py
@@ -0,0 +1,6 @@
+def main():
+    num1 = int(raw_input())
+    num2 = int(raw_input())
+    print(abs(num1 - num2))
+
+main()
